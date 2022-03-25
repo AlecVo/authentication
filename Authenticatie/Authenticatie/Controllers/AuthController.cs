2d0213de2588e5b8445d4b476e8365919757f9c7
Alec Van oosterwijck <r0850108@student.thomasmore.be>
2022-03-25 12:21:30 +0100

    Revert "Aanmaken Handtekening, createtoken()"
    This reverts commit 819549bb6a409cf583cc7786ca90cf904e22b94b.

--- Authenticatie/Authenticatie/Controllers/AuthController.cs
+++ Authenticatie/Authenticatie/Controllers/AuthController.cs
@@ -1,9 +1,6 @@
 ﻿using Authenticatie.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
 using System.Security.Cryptography;
 
 namespace Authenticatie.Controllers
@@ -13,12 +10,7 @@ namespace Authenticatie.Controllers
     public class AuthController : ControllerBase
     {
         public static User user = new User();
-        private readonly IConfiguration _configuration;
 
-        public AuthController(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
 
         [HttpPost("register")] //registreert persoon en hashed het wachtwoord
         public async Task<ActionResult<User>> Register(UserDto request)
@@ -43,31 +35,7 @@ namespace Authenticatie.Controllers
             {
                 return BadRequest("Password is wrong");
             }
-
-            string token = CreateToken(user);
-            return Ok(token);
-        }
-
-        private string CreateToken(User user)
-        {
-            List<Claim> claims = new List<Claim>
-            {
-                new Claim(ClaimTypes.Name, user.UserName)
-            };
-
-            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
-                _configuration.GetSection("AppSettings:Token").Value));
-
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
-
-            var token = new JwtSecurityToken(
-                claims: claims,
-                expires: DateTime.Now.AddDays(1),
-                signingCredentials: creds);
-
-            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
-
-            return jwt;
+            return Ok("you have logged in");
         }
 
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
@@ -83,7 +51,7 @@ namespace Authenticatie.Controllers
             using(var hmac = new HMACSHA512(passwordSalt))
             {
                 var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-                return computedHash.SequenceEqual(passwordHash); //SequenceEqual is letterlijk hetzelfde als ==, als het gehashed wachtwoord het zelfde is als het opgeslagen wachtwoord return true ander is het false
+                return computedHash.SequenceEqual(passwordHash); //SequenceEqual is letterlijk hetzelfde als ==
             }
         }
     }
