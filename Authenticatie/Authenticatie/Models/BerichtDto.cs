2e395f7739d0de490c9963e704b36e29f54d58ad
Alec Van oosterwijck <r0850108@student.thomasmore.be>
2022-03-28 12:26:30 +0200

    Want Password toegevoegd

---
+++ Authenticatie/Authenticatie/Models/BerichtDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Authenticatie.Models
+{
+    public class BerichtDto
+    {
+        public string BerichtInfo { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public bool WantPassword { get; set; }
+    }
+}
