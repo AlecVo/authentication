ff5171d8020d2320cb46fb3fe44900e845d10258
Alec Van oosterwijck <r0850108@student.thomasmore.be>
2022-03-31 13:38:46 +0200

    Nieuwe model aanmaken zodat gegevens geverifieerd kunnen worden

---
+++ Authenticatie/Authenticatie/Models/BerichtOpvragen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authenticatie.Models
+{
+    public class BerichtOpvragen
+    {
+        public string Password { get; set; } = string.Empty;
+        public byte[] PasswordSalt { get; set; }
+        public bool WantPassword { get; set; }
+    }
+}
